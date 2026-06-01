3276188133317af24c7b87571cf687785a109f1b
Samuel Hurtado Marin <samuel@hurtadom.dev>
2026-06-01 14:34:46 +0200

    gitignore and script to turn on Qemu on amd laptop

---
+++ run_qemu.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Optimized for AMD Ryzen AI 7 350 (8C/16T, 30GB RAM, KVM/AMD-V)
+# Boots from CD-ROM (ISO) first, falls back to disk
+
+IO_THREAD="iothread1"
+DISK_ID="root"
+
+qemu-system-x86_64 \
+  -name "ft_linux (shurtado)" \
+  -enable-kvm \
+  -cpu host,topoext=on \
+  -smp 12,cores=6,threads=2,sockets=1 \
+  -k es \
+  -m 16G \
+  -machine type=q35,accel=kvm,hpet=off \
+  -rtc base=utc,clock=host \
+  -global kvm-pit.lost_tick_policy=discard \
+  -object iothread,id=${IO_THREAD} \
+  -device virtio-blk-pci,drive=${DISK_ID},iothread=${IO_THREAD},num-queues=12 \
+  -drive if=none,id=${DISK_ID},file=ft_linux_disk.img,format=raw,cache=none,aio=io_uring \
+  -cdrom archlinux-2026.05.01-x86_64.iso \
+  -boot order=d \
+  -nic user,model=virtio-net-pci \
+  -vga virtio \
+  -display gtk \
+  -device virtio-tablet \
+  -audiodev none,id=audionone \
+  "$@"
